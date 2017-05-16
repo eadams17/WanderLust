2046b97380fc9c58407d453402cc26c194d00c62
eadams17 <erictayloradams@gmail.com>
2017-05-16 09:08:54 -0700

    Create SessionApiUtil ajax requests

--- frontend/wanderlust.jsx
+++ frontend/wanderlust.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, logout, signup } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
