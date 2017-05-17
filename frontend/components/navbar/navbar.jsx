ade473870544ec9d63937b94d7146d5ddb95b85b
eadams17 <erictayloradams@gmail.com>
2017-05-17 14:05:47 -0700

    Add guest demo login feature

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const guestUser = { email: 'drtobogan@gmail.com', password: 'password' };
+
 const publicNav = (login) => (
   <div className="navbar">
     <Link to="/" className="logo-link">
@@ -8,6 +10,7 @@ const publicNav = (login) => (
     </Link>
     <nav>
       <ul className="login-register-links">
+        <li><button onClick={() => login(guestUser)}>Guest</button></li>
         <li><Link to="/login">Sign In</Link></li>
         <li><Link to="/register">Register</Link></li>
       </ul>
