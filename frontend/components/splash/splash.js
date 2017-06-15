5dfa1660af33134ca0af731a10ed7ef18d833ae5
eadams17 <erictayloradams@gmail.com>
2017-06-15 13:29:55 -0700

    Add video background to splash and adjust margins for navbar

--- frontend/components/splash/splash.js
+++ frontend/components/splash/splash.js
@@ -10,6 +10,7 @@ class Splash extends React.Component {
     super(props);
 
     this.loginGuest = this.loginGuest.bind(this);
+    this.splashVideo = this.splashVideo.bind(this);
   }
 
   loginGuest (e) {
@@ -20,12 +21,29 @@ class Splash extends React.Component {
         `/profile/${res.currentUser.username}`));
   }
 
+  splashVideo() {
+    return (
+      <div className="splash-video">
+        <video
+          controls muted
+          autoPlay="autoplay"
+          loop="loop">
+          <source
+            className="mp4"
+            type="video/mp4"
+            src="https://res.cloudinary.com/explorewanderlust/video/upload/v1497557744/splash_video_g5tvoy.mp4" />
+        </video>
+      </div>
+    );
+  }
+
   render(){
 
     if (this.props.currentUser) {
       return(
         <div className="splash">
           <h2 className='splash-phrase1'>Not all those who wander are lost</h2>
+          {this.splashVideo()}
         </div>
       );
     } else {
@@ -35,6 +53,7 @@ class Splash extends React.Component {
           <div className='splash-phrase2-2'>
             <button className='splash-button' onClick={this.loginGuest}>Explore Wanderlust</button>
           </div>
+          {this.splashVideo()}
         </div>
       );
     }
