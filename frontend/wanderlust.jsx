ff8511896d8c58867baae5d752126f51991ef071
eadams17 <erictayloradams@gmail.com>
2017-07-11 23:52:35 -0400

    Remove logout function from window

--- frontend/wanderlust.jsx
+++ frontend/wanderlust.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { logout } from './actions/session_actions';
-
-window.logout = logout;
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
